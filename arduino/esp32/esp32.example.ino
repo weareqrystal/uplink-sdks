60350190375d2b8e2a2085e09a0a3797210998ba
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2026-01-13 18:26:56 +0400

    Add example code for Qrystal SDK implementation on ESP32

---
+++ arduino/esp32/esp32.example.ino
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+// INCLUDE THE LIBRARY
+#include "qrystal.hpp"
+
+// TODO: SET WIFI NAME AND PASSWORD
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Initialize Qrystal once
+Qrystal q;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    int n = WiFi.scanNetworks();
+    if (n <= 0)
+    {
+        Serial.println("error: no wifi networks available");
+        WiFi.disconnect();
+        return;
+    }
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("wifi connected. local ip is: " + WiFi.localIP());
+}
+
+void loop()
+{
+    Serial.begin(115200);
+
+    // put your main code here, to run repeatedly:
+    for (;;)
+    {
+        // REPLACE DEVICE ID WITH YOUR DEVICE'S ID AND SET THE TOKEN
+        Qrystal::QRYSTAL_STATE s = q.heartbeat("<DEVICE_ID>:<DEVICE_TOKEN>");
+        // optionally, handle errors here
+        // if (s != Q_OK) {
+        //
+        // }
+
+        Serial.println(WiFi.localIP());
+        delay(1000);
+    }
+}
